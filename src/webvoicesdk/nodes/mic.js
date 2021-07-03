f01554a6daf9c2ffc0115008864709239acdcab4
Damien Laine <damien.laine@gmail.com>
2021-07-03 15:09:03 +0200

    Mic.start() must now get called before hooking nodes Removed DummyAudioContext to fetch the samplerate before starting a real microphone bound audioCOntext

--- src/webvoicesdk/nodes/mic.js
+++ src/webvoicesdk/nodes/mic.js
@@ -3,7 +3,6 @@ import NodeError from '../nodes/error.js'
 
 export default class Mic extends Node {
     static defaultOptions = {
-        //sampleRate: 44100, 
         frameSize: 4096,
         constraints: {
             echoCancellation: true,
@@ -13,7 +12,6 @@ export default class Mic extends Node {
     }
 
     constructor({
-        //sampleRate = 44100,
         frameSize = 4096,
         constraints = {
             echoCancellation: true,
@@ -22,37 +20,26 @@ export default class Mic extends Node {
         }
     } = {}) {
         super()
-        // For cross-browser compat, i will now use only default sampleRate.
-        // I instanciante a Dummy AudioContext to fetch the browser / OS / Device default value
-        //@TODO : CLEAN THIS DIRTY HACK. Quite complicated though...
         this.hookableOnNodeTypes = [] //none, this node will connect to getUserMedia stream
-        let DummyAudioContext = window.AudioContext || window.webkitAudioContext
-        let dummyAudioCtx = new DummyAudioContext()
         this.type = "mic"
         this.event = "micFrame" //emitted
         this.hookedOn = null
         this.options = {
-            sampleRate: dummyAudioCtx.sampleRate,
             frameSize,
             constraints
         }
-        dummyAudioCtx.close()
     }
 
     async start() {
-        if (this.hookedOn) throw new NodeError(`node ${this.type} is already hooked, call stop() first`)
+        if (this.hookedOn) throw new NodeError(`Microphone is already started, call stop() first`)
         this.stream = await navigator.mediaDevices.getUserMedia({
             audio: {
-                sampleRate: this.options.sampleRate,
                 channelCount: 1,
                 ...this.options.constraints
             },
         })
         this.hookedOn = true
-
-        this.audioContext = new(window.AudioContext || window.webkitAudioContext)({
-            sampleRate: this.options.sampleRate,
-        })
+        this.audioContext = new(window.AudioContext || window.webkitAudioContext)()
         this.options.sampleRate = this.audioContext.sampleRate
         this.mediaStreamSource = this.audioContext.createMediaStreamSource(this.stream)
         this.micFrameGenerator = this.audioContext.createScriptProcessor(this.options.frameSize, 1, 1)
